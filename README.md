d729c25c0b5eea555dbef197ea046a69f6eb5ac3
Elyager <elyager@gmail.com>
2018-01-29 15:46:02 -0700

    Inital package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Quotes
+
+A small library to get quotes from [GoodRead](https://www.goodreads.com/quotes) and maybe another websites later, maybe not.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'tiny-quotes',
+  package = ['tiny-package'],
+  version = '0.1',
+  description = 'This packages allows you to bring random quotes from www.goodreads.com',
+  author = 'Erik Ochoa',
+  author_email = 'elyager@gmail.com',
+  url = ''
+  keyword = ['quotes', 'motivation', 'utils']
+)
