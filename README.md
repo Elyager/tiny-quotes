f97846bf1b5d0798dba24aaac018fee1d2dc478f
Elyager <elyager@gmail.com>
2018-01-30 20:32:55 -0700

    [Fix] remove old configuration and use twine

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Tiny Quotes
 
 A small library to get quotes from [GoodReads](https://www.goodreads.com/quotes) and maybe another websites later, maybe not.
+
+To generate a new version of the package, change the version number in the `setup.py` file and run the following commands:
+
+```bash
+$ python3 setup.py sdist 
+```
+```bash
+$ twine upload dist/[Filename]
+```
+
+**Reference:**
+- [Upload a package in pipy using twine](https://anweshadas.in/how-to-upload-a-package-in-pypi-using-twine/)
--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(
-  name = "tiny-quotes",
-  package = ["tiny-quotes"],
-  version = "0.1",
-  description = "A small library to get quotes from www.goodread.com and maybe another websites later, maybe not.",
-  author = "Erik Ochoa",
-  author_email = "elyager@gmail.com",
-  url = "https://github.com/Elyager/tiny-quotes",
-  keyword = ["quotes", "utils"]
+setup(  
+    name="tQuotes",
+    version='0.0.3',
+    description = 'A library to get quotes from www.goodread.com.',
+    long_description="A library to get quotes from www.goodread.com and maybe another websites later, maybe... maybe not.",
+    author="Erik Ochoa",
+    author_email="elyager@gmail.com",
+    url="https://github.com/Elyager/tQuotes",
+    license="MIT",
+    packages=['quotes'],
+    install_requires=[
+        'beautifulsoup4==4.6.0',
+        'requests==2.18.4'
+    ]
 )
